7b6189106165fb23cc1f02366b872bbc23164ae6
Ayush Pateria <cs15btech11007@iith.ac.in>
2019-07-24 01:25:42 +0530

    Add goroutine based qps

--- counter.go
+++ counter.go
@@ -7,36 +7,12 @@ import (
 
 type counter struct {
 	val	uint64
-	// byTs is a mapping from timestamp to count, it
-	// stores the count of the counter at the end of that
-	// second, and is used to calculate qps. We only store
-	// latest three timestamps in byTs, as only last two
-	// seconds are required to calculate qps.
-	byTs map[int64]uint64
+	q *qps
 }
 
-// inc increments the value of counter by 1. It also
-// stores the current count to `byTs` for qps calculation
-// To make sure multiple threads are able to operate on the
-// counter at the same time, atomic increments are used.
+// inc increments the value of counter by 1. 
 func (c *counter) inc() {
-	currVal := atomic.AddUint64(&c.val, 1)
-	// Storing latest three seconds in byTs for qps calculation.
-	ts := time.Now().Unix()
-	_, ok := c.byTs[ts-1]
-	// If there was no increment in the previous second,
-	// assign previous two seconds from the current value.
-	if !ok {
-		prevVal := currVal - 1
-		c.byTs[ts-1] = prevVal
-		c.byTs[ts-2] = prevVal
-	}
-	// Reassigning map with the latest three seconds.
-	c.byTs = map[int64]uint64 {
-		ts: currVal,
-		ts-1: c.byTs[ts-1],
-		ts-2: c.byTs[ts-2],
-	}
+	atomic.AddUint64(&c.val, 1)
 }
 
 // value returns the current value of the counter, it does
@@ -45,15 +21,37 @@ func (c *counter) value() uint64 {
 	return atomic.LoadUint64(&c.val)
 }
 
+func (c *counter) updateQps() {
+	currCount := c.value()
+	c.q.updateCounts(currCount)
+}
+
 // qps returns the number of requests served in the last second.
 func (c *counter) qps() uint64 {
-	ts := time.Now().Unix()
-	if c.byTs[ts-1] == 0 {
-		return 0
-	}
-	return c.byTs[ts-1] - c.byTs[ts-2]
+	return c.q.getQps()
 }
 
 func newCounter() *counter {
-	return &counter{byTs: make(map[int64]uint64)}
+	c := &counter{q: &qps{}}
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			c.updateQps()
+		}
+	}()
+	return c
+}
+
+type qps struct {
+	currCount uint64
+	lastSecCount uint64
+}
+
+func (q *qps) updateCounts(currCount uint64) {
+	q.lastSecCount = q.currCount
+	q.currCount = currCount
+}
+
+func (q *qps) getQps() uint64 {
+	return q.currCount - q.lastSecCount 
 }
