a536142c402e2f75377e633681c8ab69c6dcda0d
Ayush Pateria <cs15btech11007@iith.ac.in>
2019-07-24 01:44:46 +0530

    Add qps test

--- counter.go
+++ counter.go
@@ -7,6 +7,7 @@ import (
 
 type counter struct {
 	val	uint64
+	// q stores the information to calculate qps
 	q *qps
 }
 
@@ -21,6 +22,7 @@ func (c *counter) value() uint64 {
 	return atomic.LoadUint64(&c.val)
 }
 
+// update the counts for qps calculation
 func (c *counter) updateQps() {
 	currCount := c.value()
 	c.q.updateCounts(currCount)
@@ -33,6 +35,8 @@ func (c *counter) qps() uint64 {
 
 func newCounter() *counter {
 	c := &counter{q: &qps{}}
+	// Start a go routine which will capture the current
+	// value of counter for qps calculation.
 	go func() {
 		for {
 			time.Sleep(1 * time.Second)
@@ -42,11 +46,14 @@ func newCounter() *counter {
 	return c
 }
 
+// qps tracks the value of current and last second counter values.
 type qps struct {
 	currCount uint64
 	lastSecCount uint64
 }
 
+// updateCounts updates the curr and last count, this
+// is called every second from counter.
 func (q *qps) updateCounts(currCount uint64) {
 	q.lastSecCount = q.currCount
 	q.currCount = currCount
--- counter_test.go
+++ counter_test.go
@@ -1,7 +1,9 @@
 package main
 
-import "testing"
-
+import (
+	"testing"
+	"time"
+)
 func TestCounter(t *testing.T) {
 	c := newCounter()
 	if expected, got := uint64(0), c.value(); expected != got {
@@ -9,6 +11,19 @@ func TestCounter(t *testing.T) {
 	}
 	c.inc()
 	if expected, got := uint64(1), c.value(); expected != got {
+		t.Errorf("Expected %d, got %d.", expected, got)
+	}
+}
+
+func TestQps(t *testing.T) {
+	c := newCounter()
+	if expected, got := uint64(0), c.qps(); expected != got {
+		t.Errorf("Expected %d, got %d.", expected, got)
+	}
+	c.inc()
+	c.inc()
+	time.Sleep(1 * time.Second)
+	if expected, got := uint64(2), c.qps(); expected != got {
 		t.Errorf("Expected %d, got %d.", expected, got)
 	}
 }
